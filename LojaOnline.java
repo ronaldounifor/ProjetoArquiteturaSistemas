6b6f01ab86f48a4f8c4fbc1ec8e34ba2a71292ae
professord14 <pr0fl@bd14>
2022-08-03 09:36:34 -0300

    Primeiro commit (adicionando classe LojaOnline)

---
+++ LojaOnline.java
@@ -0,0 +1,9 @@
+public class LojaOnline {
+    public void efetuarPedido(){}
+    public void imprimirCupomFiscal(){}
+    public void calcularFrete(){}
+    public void cancelarPedido(){}
+    public void adicionarAoCarrinho(){}
+    public void limparCarrinho(){}
+    public void visualizarPedido(){}
+}
